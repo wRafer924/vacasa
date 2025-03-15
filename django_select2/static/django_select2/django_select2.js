9927be2f4cdd601f61c3273d41442abfe1da8cae
armen58 <markosarmen5@gmail.com>
2025-03-15 17:50:37 +0100

    Fix  #136  -- Support dependent fields in formsets (#316)
    * Find the closest common ancestor of two chained selects instead of
    assuming it is a form.
    * Try to find the dependent field by its name attribute using a two-fold
    search strategy: exact match otherwise ends with
    
    Special thanks to @rez0n

--- django_select2/static/django_select2/django_select2.js
+++ django_select2/static/django_select2/django_select2.js
@@ -26,9 +26,16 @@
 
           let dependentFields = $element.data('select2-dependent-fields')
           if (dependentFields) {
+            const findElement = function (selector) {
+              const result = $(selector, $element.closest(`:has(${selector})`))
+              if (result.length > 0) return result
+              else return null
+            }
             dependentFields = dependentFields.trim().split(/\s+/)
             $.each(dependentFields, function (i, dependentField) {
-              result[dependentField] = $('[name=' + dependentField + ']', $element.closest('form')).val()
+              const nameIs = `[name=${dependentField}]`
+              const nameEndsWith = `[name$=-${dependentField}]`
+              result[dependentField] = (findElement(nameIs) || findElement(nameEndsWith)).val()
             })
           }
 
